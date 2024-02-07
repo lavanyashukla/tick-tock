0c0e3b080f111760fc136eb833bfe66e69bfa1ba
Lavanya Shukla <lavanyashukla@Lavanyas-MacBook-Pro-2.local>
2024-02-07 15:33:49 -0800

    add basic eval

--- classify.py
+++ classify.py
@@ -6,11 +6,6 @@ import os
 input_file = open('data/support_tickets.json')
 data = json.load(input_file)
 
-# what am I looking at
-for element in data: 
-    # print(element)
-    break
-
 client = OpenAI(
     # This is the default and can be omitted
     api_key=os.environ["OPENAI_API_KEY"]
@@ -28,47 +23,56 @@ with open(file_path, 'r') as file:
     priorities = [line.strip() for line in file]
 # print(tags)
 
-ticket_text = element['description']
 examples = "".join([f"Text: {element['description']}\nPriority: {element['priority']}\n" for i, element in enumerate(data[:5])])
+count, score_priority, score_tags = 0, 0
 
-# Prompt – Classify Priority
-user_prompt_priority = f"""
-Classify the text into one of the following classes.
-Classes: {priorities}
-{examples}
-Text: {ticket_text}
-Priority: """
+for element in data[100:110]:
+    # print(element)
+    ticket_text = element['description']
 
-# print("Prompt: "+user_prompt_priority)
+    # Prompt – Classify Priority
+    user_prompt_priority = f"""
+    Classify the text into one of the following classes.
+    Classes: {priorities}
+    {examples}
+    Text: {ticket_text}
+    Priority: """
+    # print("Prompt: "+user_prompt_priority)
 
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": user_prompt_priority},
-    ],
-    max_tokens=100
-)
-print("Predicted Priority: \n"+response.choices[0].message.content)
-print("Actual Priority:")
-print(element['priority'])
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": user_prompt_priority},
+        ],
+        max_tokens=100
+    )
+    print("Predicted Priority: \n"+response.choices[0].message.content)
+    print("Actual Priority:")
+    print(element['priority'])
+    print("__________________")
+    if(response.choices[0].message.content == element['priority']):
+        score_priority += 1
+    count += 1
+
+    # Prompt – Classify Class
+    user_prompt_tag = f"""
+    Classify the text into one of the following classes.
+    Classes: {tags}
+    Text: {ticket_text}
+    Class: """
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": user_prompt_tag},
+        ],
+        max_tokens=100
+    )
+    print("Predicted Tags: \n"+response.choices[0].message.content)
+    print("Actual Tags:")
+    print(element['tags'])
+
 print("__________________")
+print(f"Priority Accuracy: {score_priority/count}")
 
-
-# Prompt – Classify Class
-user_prompt_tag = f"""
-Classify the text into one of the following classes.
-Classes: {tags}
-Text: {ticket_text}
-Class: """
-
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": user_prompt_tag},
-    ],
-    max_tokens=100
-)
-print("Predicted Tag: \n"+response.choices[0].message.content)
-print("Actual Tag:")
-print(element['tags'])
 
